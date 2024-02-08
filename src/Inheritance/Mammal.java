1624fb95f453bb989d912df0bb4f4f7fc57ffb05
Ian <a16782@macbook-air-2.lan>
2024-02-07 20:50:50 -0800

    Animal

--- src/Inheritance/Mammal.java
+++ src/Inheritance/Mammal.java
@@ -1,2 +1,32 @@
-package Inheritance;public class Mammal {
+package Inheritance;
+
+public class Mammal extends Animal{
+
+    private boolean hasFur;
+    private boolean giveBirth;
+
+    public Mammal() {
+        super(false, 2, "Omnivore");
+
+    }
+
+
+    public Mammal(boolean hasFur, boolean giveBirth){
+        super(false,2, "Carnivore");
+        this.hasFur = hasFur;
+        this.giveBirth = giveBirth;
+
+    }
+
+    @Override
+    public void makeNoise() {
+        super.makeNoise();
+        System.out.println(" Meow~~~");
+    }
+
+    @Override
+    public void eat() {
+        super.eat();
+        System.out.println(" Yummy");
+    }
 }
--- src/Inheritance/Reptile.java
+++ src/Inheritance/Reptile.java
@@ -1,2 +1,43 @@
-package Inheritance;public class Reptile {
+package Inheritance;
+
+import javax.swing.*;
+
+public class Reptile extends Animal{
+    private boolean layEggs;
+    private String color;
+    private boolean hasVenom;
+    private boolean coldBlooded;
+
+    public Reptile(boolean coldBlooded){
+        this();
+        this.coldBlooded = coldBlooded;
+    }
+
+    public Reptile() {
+        super(true,0,"Carnivore");
+        this.layEggs = false;
+        this.hasVenom = true;
+        this.color = "red";
+        this.coldBlooded = true;
+    }
+
+    public Reptile(boolean layEggs, String color, boolean hasVenom){
+        super(true, 0, "Carnivore");
+        this.color = color;
+        this.layEggs = layEggs;
+        this.hasVenom = hasVenom;
+    }
+
+
+    @Override
+    public void eat(){
+        super.eat();
+        System.out.println(" Yummy");
+    }
+
+    @Override
+    public void makeNoise(){
+        super.makeNoise();
+        System.out.println(" sssssss");
+    }
 }
