2e84dc8dca4f9fcd11881f17e1b76df966cefed3
Ian <a16782@MacBook-Air-2.local>
2024-02-12 08:42:12 -0800

    Animal abd Super demo

---
+++ src/Demonstrate/Car.java
@@ -0,0 +1,34 @@
+package Demonstrate;
+
+public class Car extends Transportation{
+    private String model;
+    private int Year;
+
+    public Car(String model, int year) {
+        super();
+        this.model = model;
+        Year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return Year;
+    }
+
+    public void setYear(int year) {
+        Year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car : " + model + Year + super.toString() ;
+    }
+}
+
